e2309491f32c96d32f35131be0223abc7f225a47
Thomas Pretterhofer <thomas.Pretterhofer@gmail.com>
2019-03-26 14:29:46 +0100

    added adios

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ public class Main {
 	
 	
 	public static void sayHello(Gretter g){
-		System.out.printf("Adios brainfuck, %s", g.getName());
+		System.out.printf("Awesome Method!");
+		System.out.printf("Chellas, %s", g.getName());
 	}
 
 
